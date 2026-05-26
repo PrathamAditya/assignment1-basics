12dbb348c878cf11f36b9f09a9941c3ffca1ec29
PrathamAditya <prathamadityasalhotra@gmail.com>
2026-05-26 15:43:32 +0530

    Softmax Implementation Completed.

---
+++ cs336_basics/softmax.py
@@ -0,0 +1,13 @@
+import torch
+
+
+def Softmax(input: torch.tensor, dim: int):
+    if dim > input.ndim:
+        return input    
+
+    max_values, _ = torch.max(input, dim=dim, keepdim=True)
+    stable_input = input - max_values
+    exp_tensor = torch.exp(stable_input)
+    sum_exp = torch.sum(exp_tensor, dim=dim, keepdim=True)
+    return exp_tensor / sum_exp
+    
